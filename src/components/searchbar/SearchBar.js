e07e8b1482a6ab43f5418fa4cc18d68f121f10c7
kakarot98 <hrushikeshmohanty93@gmail.com>
2020-08-18 22:10:26 +0530

    added navbar

--- src/components/searchbar/SearchBar.js
+++ src/components/searchbar/SearchBar.js
@@ -1,10 +1,24 @@
 import React, { useState } from "react";
-import TextField from 'material-ui/TextField'
-import SelectField from 'material-ui/SelectField'
+import TextField from "material-ui/TextField";
+import SelectField from "material-ui/SelectField";
 
 export default function SearchBar() {
-const API_KEY = '17948866-a8bea270ce16ae94ad9ee0846'
-     
+  const API_KEY = "17948866-a8bea270ce16ae94ad9ee0846";
+  const url = "https://pixabay.com/api";
+
   const [searchtext, setSearchText] = useState("");
-  return <div></div>;
+  const [amount, setAmount] = useState("");
+  const [images, setImages] = useState([]);
+  return (
+    <div>
+      <TextField
+        name="searchtext"
+        value={searchtext}
+        onChange={(e) => {
+          setSearchText(e.target.value);
+        }}
+        placeholder="Search for images"
+      />
+    </div>
+  );
 }
