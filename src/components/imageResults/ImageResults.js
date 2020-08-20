c99c10ac289a5eef89976e986e1b1767ee10b97f
Hrushikesh Mohanty <hrushikeshmohanty90@gmail.com>
2020-08-20 13:21:33 +0530

    added image list

--- src/components/imageResults/ImageResults.js
+++ src/components/imageResults/ImageResults.js
@@ -1,10 +1,43 @@
 import React from "react";
-import { GridList, GridListTile, IconButton, Dialog, Button } from '@material-ui/core'
-import { ZoomIn } from '@material-ui/icons'
+import {
+  GridList,
+  GridListTileBar,
+  IconButton,
+  Dialog,
+  Button,
+} from "@material-ui/core";
+import { ZoomIn } from "@material-ui/icons";
 
-const ImageResults = (images) => {
-  return <div></div>;
+const ImageResults = ({ images }) => {
+  let imageContentList;
+  if (images) {
+    imageContentList = (
+      <GridList cols={3}>
+        {images.map((img) => (
+          <GridListTileBar
+            title={img.tags}
+            key={img.id}
+            subtitle={
+              <span>
+                by <strong>{img.user}</strong>
+              </span>
+            }
+            actionIcon={
+              <IconButton>
+                <ZoomIn color="aqua" />
+              </IconButton>
+            }
+          >
+            <img src={img.largeImageURL} alt="" />
+          </GridListTileBar>
+        ))}
+      </GridList>
+    );
+  } else {
+    imageContentList = null;
+  }
+
+  return <div>{imageContentList}</div>;
 };
-
 
 export default ImageResults;
